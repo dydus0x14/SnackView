ceab3dd9d8e4da15069ce76949e1c04c2cbe2754
dydus0x14 <dydus0x14@gmail.com>
2016-02-07 14:06:30 +0300

    Updated readme and project settings

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SnackView
+Implementation UIStackView using Cartography ( https://github.com/robb/Cartography ).
+SnackView supports iOS 8 and contains IBInspectable elements for Storyboard.
--- SnackView/SnackView.swift
+++ SnackView/SnackView.swift
@@ -9,7 +9,7 @@
 * "THE BEER-WARE LICENSE" (Revision 42):
 * dydus0x14 wrote this file.  As long as you retain this notice you
 * can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a beer in return.   
+* this stuff is worth it, you can buy me a beer in return Anton Davydov.
 * ----------------------------------------------------------------------------
 */
 
